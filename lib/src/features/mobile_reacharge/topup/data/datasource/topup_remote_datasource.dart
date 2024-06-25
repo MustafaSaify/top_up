7294a53691eb7d00f21eacb2a2375a0921bee395
MustafaSaify <mustafa.saify04@gmail.com>
2024-06-25 09:05:04 +0400

    added minor comments

--- lib/src/features/mobile_reacharge/topup/data/datasource/topup_remote_datasource.dart
+++ lib/src/features/mobile_reacharge/topup/data/datasource/topup_remote_datasource.dart
@@ -12,6 +12,7 @@ abstract class TopUpRemoteDataSource {
 class TopupRemoteDataSourceImpl extends TopUpRemoteDataSource {
   @override
   Future<List<TopUpOption>> getTopUpOptions() async {
+    // Mocked response.
     final topUpOptions = [
       TopUpOption(amount: 5, currencyCode: 'AED'),
       TopUpOption(amount: 10, currencyCode: 'AED'),
@@ -26,6 +27,7 @@ class TopupRemoteDataSourceImpl extends TopUpRemoteDataSource {
 
   @override
   Future<TopUpConfig> getTopUpConfig() async {
+    // Mocked response.
     return TopUpConfig(
         serviceCharge: 1,
         verifiedUserLimit: 1000,
@@ -37,6 +39,7 @@ class TopupRemoteDataSourceImpl extends TopUpRemoteDataSource {
 
   @override
   Future<TopUpSummary> initiateTopUp(String beneficairyId, num amount) async {
+    // Mocked response.
     return TopUpSummary(
       transactionId: '2a011e5d-18f7-4410-86a6',
       beneficiaryName: 'Amit Pahandit',
@@ -50,6 +53,7 @@ class TopupRemoteDataSourceImpl extends TopUpRemoteDataSource {
 
   @override
   Future<void> completeTopUp(String transactionId) async {
+    // Mocked response.
     return;
   }
 }
