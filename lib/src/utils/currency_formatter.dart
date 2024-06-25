a52d4c0f1d057af73c61217f7ca052fd528c919b
MustafaSaify <mustafa.saify04@gmail.com>
2024-06-25 13:24:16 +0400

    added a todo to currency formatter

--- lib/src/utils/currency_formatter.dart
+++ lib/src/utils/currency_formatter.dart
@@ -1,11 +1,9 @@
-import 'dart:io';
-import 'package:intl/intl.dart';
-
 abstract class CurrencyFormatter {
   String formatCurrency(num amount, String currencyCode);
 }
 
 class CurrencyFormatterImpl extends CurrencyFormatter {
+  // TODO: Refine this logic to cater for decimal places and currency symbols.
   @override
   String formatCurrency(num amount, String currencyCode) {
     return '$currencyCode $amount';
